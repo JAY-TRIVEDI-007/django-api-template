44e9240a7179f49ea0bd4c7e2dd24b3f2cb43724
Jay Trivedi <61327228+JAY-TRIVEDI-007@users.noreply.github.com>
2024-07-27 17:51:36 +0530

    Update: Token authentication replaced with basic and first, last name fields make required.

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -21,6 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("djoser.urls.base")),
+    path("api/auth/", include("djoser.urls.authtoken")),
     path("api/auth/", include("djoser.urls.jwt")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
